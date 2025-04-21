7be7bbd01f22a431fb1ce700242713a1a979d11e
jacobzion <jacob.zion.lim@gmail.com>
2025-04-21 10:08:32 -0700

    Create layout.tsx

---
+++ app/layout.tsx
@@ -0,0 +1,10 @@
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <head>
+        <title>Axion Capitals</title>
+      </head>
+      <body>{children}</body>
+    </html>
+  );
+}
