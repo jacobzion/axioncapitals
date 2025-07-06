1132e1fc675dfe096e892580144493379998b645
jacobzion <jacob.zion.lim@gmail.com>
2025-07-05 17:52:49 -0700

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,11 @@
-// tailwind.config.js
+/** @type {import('tailwindcss').Config} */
 module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
-    extend: {
-      colors: {
-        sapphire: '#0F172A',
-        royalgold: '#D4AF37',
-        skymist: '#E2E8F0',
-        slategray: '#334155',
-        azure: '#3B82F6',
-      },
-    },
+    extend: {},
   },
-};
+  plugins: [],
+}
